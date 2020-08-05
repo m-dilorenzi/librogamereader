e5bbfd886b73fa68836b2c17e0e1dccd4dc2b484
Matteo Di Lorenzi <matteo.dilorenzi98@gmail.com>
2020-08-05 11:49:06 +0200

    Aggiunti moduli e modificato file server.js

--- server.js
+++ server.js
@@ -1,16 +1,159 @@
-const express = require('express');
-const { ExpressAdapter } = require('ask-sdk-express-adapter');
 
-const app = express();
-const skillBuilder = Alexa.SkillBuilders.custom();
-const skill = skillBuilder.create();
-const adapter = new ExpressAdapter(skill, true, true);
+let express = require('express'),
+  bodyParser = require('body-parser'),
+  port = process.env.PORT || 8080,
+  app = express();
+let alexaVerifier = require('alexa-verifier');
 
-const PORT      = process.env.PORT;
-const listener  = server.listen(PORT, () =>
-    console.log(`The server is listening on port: ${listener.address().port}`)
-);
+app.listen(port);
 
-app.post('/', adapter.getRequestHandlers(), function(req, res){
-    console.log(req);
+console.log('Server is listening on port: ' + port);
+
+var isFisrtTime = true;
+
+const SKILL_NAME = 'LibroGameReader';
+const HELP_MESSAGE = 'Ciao, ora puoi implementare la vera struttura!';
+const STOP_MESSAGE = 'Ciao, alla prossima!';
+const PAUSE = '<break time="0.3s" />'
+const WHISPER = '<amazon:effect name="whispered"/>'
+const HELP_REPROMPT = 'What can I help you with?';
+
+app.use(bodyParser.json({
+  verify: function getRawBody(req, res, buf) {
+    req.rawBody = buf.toString();
+  }
+}));
+
+function requestVerifier(req, res, next) {
+  alexaVerifier(
+    req.headers.signaturecertchainurl,
+    req.headers.signature,
+    req.rawBody,
+    function verificationCallback(err) {
+      if (err) {
+        res.status(401).json({
+          message: 'Verification Failure',
+          error: err
+        });
+      } else {
+        next();
+      }
+    }
+  );
+}
+
+function log() {
+  if (true) {
+    console.log.apply(console, arguments);
+  }
+}
+
+app.post('/', requestVerifier, function(req, res) {
+
+  if (req.body.request.type === 'LaunchRequest') {
+    res.json(getNewHero());
+    isFisrtTime = false
+  } else if (req.body.request.type === 'SessionEndedRequest') { /* ... */
+    log("Session End")
+  } else if (req.body.request.type === 'IntentRequest') {
+    switch (req.body.request.intent.name) {
+      case 'AMAZON.YesIntent':
+        res.json(getNewChapter());
+        break;
+      case 'AMAZON.NoIntent':
+        res.json(stopAndExit());
+        break;
+      case 'AMAZON.HelpIntent':
+        res.json(help());
+        break;
+      default:
+
+    }
+  }
 });
+
+function handleDataMissing() {
+  return buildResponse(MISSING_DETAILS, true, null)
+}
+
+function stopAndExit() {
+
+  const speechOutput = STOP_MESSAGE
+  var jsonObj = buildResponse(speechOutput, true, "");
+  return jsonObj;
+}
+
+function help() {
+
+  const speechOutput = HELP_MESSAGE
+  const reprompt = HELP_REPROMPT
+  var jsonObj = buildResponseWithRepromt(speechOutput, false, "", reprompt);
+
+  return jsonObj;
+}
+
+function getNewChapter() {
+
+  var welcomeSpeechOutput = 'Benvenuto su libro game reader! <break time="0.3s" />'
+  if (!isFisrtTime) {
+    welcomeSpeechOutput = '';
+  }
+
+  const tempOutput = WHISPER + HELP_MESSAGE + PAUSE;
+  const speechOutput = welcomeSpeechOutput + tempOutput;
+  
+  return buildResponseWithRepromt(speechOutput, false, HELP_MESSAGE, '');
+
+}
+
+function buildResponse(speechText, shouldEndSession, cardText) {
+
+  const speechOutput = "<speak>" + speechText + "</speak>"
+  var jsonObj = {
+    "version": "1.0",
+    "response": {
+      "shouldEndSession": shouldEndSession,
+      "outputSpeech": {
+        "type": "SSML",
+        "ssml": speechOutput
+      },
+      "card": {
+        "type": "Simple",
+        "title": SKILL_NAME,
+        "content": cardText,
+        "text": cardText
+      }
+    }
+  }
+  return jsonObj
+}
+
+function buildResponseWithRepromt(speechText, shouldEndSession, cardText, reprompt) {
+
+  const speechOutput = "<speak>" + speechText + "</speak>"
+  var jsonObj = {
+     "version": "1.0",
+     "response": {
+      "shouldEndSession": shouldEndSession,
+       "outputSpeech": {
+         "type": "SSML",
+         "ssml": speechOutput
+       },
+     "card": {
+       "type": "Simple",
+       "title": SKILL_NAME,
+       "content": cardText,
+       "text": cardText
+     },
+     "reprompt": {
+       "outputSpeech": {
+         "type": "PlainText",
+         "text": reprompt,
+         "ssml": reprompt
+       }
+     }
+   }
+ }
+  return jsonObj
+}
+
