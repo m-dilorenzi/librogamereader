21f2da2149d3262180c15f2e30c09b79a025b013
Matteo Di Lorenzi <matteo.dilorenzi98@gmail.com>
2020-08-05 11:57:02 +0200

    Modificato file server.js che stampa ora la richiesta ricevuta

--- server.js
+++ server.js
@@ -19,141 +19,143 @@ const WHISPER = '<amazon:effect name="whispered"/>'
 const HELP_REPROMPT = 'What can I help you with?';
 
 app.use(bodyParser.json({
-  verify: function getRawBody(req, res, buf) {
-    req.rawBody = buf.toString();
-  }
+    verify: function getRawBody(req, res, buf) {
+        req.rawBody = buf.toString();
+    }
 }));
 
 function requestVerifier(req, res, next) {
-  alexaVerifier(
-    req.headers.signaturecertchainurl,
-    req.headers.signature,
-    req.rawBody,
-    function verificationCallback(err) {
-      if (err) {
-        res.status(401).json({
-          message: 'Verification Failure',
-          error: err
-        });
-      } else {
-        next();
-      }
-    }
-  );
+    alexaVerifier(
+        req.headers.signaturecertchainurl,
+        req.headers.signature,
+        req.rawBody,
+        function verificationCallback(err) {
+        if (err) {
+            res.status(401).json({
+            message: 'Verification Failure',
+            error: err
+            });
+        } else {
+            next();
+        }
+        }
+    );
 }
 
 function log() {
-  if (true) {
-    console.log.apply(console, arguments);
-  }
+    if (true) {
+        console.log.apply(console, arguments);
+    }
 }
 
 app.post('/', requestVerifier, function(req, res) {
 
-  if (req.body.request.type === 'LaunchRequest') {
-    res.json(getNewHero());
-    isFisrtTime = false
-  } else if (req.body.request.type === 'SessionEndedRequest') { /* ... */
-    log("Session End")
-  } else if (req.body.request.type === 'IntentRequest') {
-    switch (req.body.request.intent.name) {
-      case 'AMAZON.YesIntent':
-        res.json(getNewChapter());
-        break;
-      case 'AMAZON.NoIntent':
-        res.json(stopAndExit());
-        break;
-      case 'AMAZON.HelpIntent':
-        res.json(help());
-        break;
-      default:
+    console.log(req);
 
+    if (req.body.request.type === 'LaunchRequest') {
+        res.json(getNewHero());
+        isFisrtTime = false
+    } else if (req.body.request.type === 'SessionEndedRequest') { /* ... */
+        log("Session End")
+    } else if (req.body.request.type === 'IntentRequest') {
+        switch (req.body.request.intent.name) {
+        case 'AMAZON.YesIntent':
+            res.json(getNewChapter());
+            break;
+        case 'AMAZON.NoIntent':
+            res.json(stopAndExit());
+            break;
+        case 'AMAZON.HelpIntent':
+            res.json(help());
+            break;
+        default:
+
+        }
     }
-  }
 });
 
-function handleDataMissing() {
-  return buildResponse(MISSING_DETAILS, true, null)
+function handleDataMissing() 
+{
+    return buildResponse(MISSING_DETAILS, true, null)
 }
 
-function stopAndExit() {
-
-  const speechOutput = STOP_MESSAGE
-  var jsonObj = buildResponse(speechOutput, true, "");
-  return jsonObj;
+function stopAndExit() 
+{
+    const speechOutput = STOP_MESSAGE
+    var jsonObj = buildResponse(speechOutput, true, "");
+    return jsonObj;
 }
 
-function help() {
+function help() 
+{
+    const speechOutput = HELP_MESSAGE
+    const reprompt = HELP_REPROMPT
+    var jsonObj = buildResponseWithRepromt(speechOutput, false, "", reprompt);
 
-  const speechOutput = HELP_MESSAGE
-  const reprompt = HELP_REPROMPT
-  var jsonObj = buildResponseWithRepromt(speechOutput, false, "", reprompt);
-
-  return jsonObj;
+    return jsonObj;
 }
 
-function getNewChapter() {
+function getNewChapter() 
+{
+    var welcomeSpeechOutput = 'Benvenuto su libro game reader! <break time="0.3s" />'
+    if (!isFisrtTime) {
+        welcomeSpeechOutput = '';
+    }
 
-  var welcomeSpeechOutput = 'Benvenuto su libro game reader! <break time="0.3s" />'
-  if (!isFisrtTime) {
-    welcomeSpeechOutput = '';
-  }
-
-  const tempOutput = WHISPER + HELP_MESSAGE + PAUSE;
-  const speechOutput = welcomeSpeechOutput + tempOutput;
-  
-  return buildResponseWithRepromt(speechOutput, false, HELP_MESSAGE, '');
-
+    const tempOutput = WHISPER + HELP_MESSAGE + PAUSE;
+    const speechOutput = welcomeSpeechOutput + tempOutput;
+    
+    return buildResponseWithRepromt(speechOutput, false, HELP_MESSAGE, '');
 }
 
 function buildResponse(speechText, shouldEndSession, cardText) {
 
-  const speechOutput = "<speak>" + speechText + "</speak>"
-  var jsonObj = {
-    "version": "1.0",
-    "response": {
-      "shouldEndSession": shouldEndSession,
-      "outputSpeech": {
-        "type": "SSML",
-        "ssml": speechOutput
-      },
-      "card": {
+    const speechOutput = "<speak>" + speechText + "</speak>"
+    var jsonObj = {
+        "version": "1.0",
+        "response": {
+        "shouldEndSession": shouldEndSession,
+        "outputSpeech": {
+            "type": "SSML",
+            "ssml": speechOutput
+        },
+        "card": {
+            "type": "Simple",
+            "title": SKILL_NAME,
+            "content": cardText,
+            "text": cardText
+        }
+        }
+    }
+    return jsonObj;
+}
+
+function buildResponseWithRepromt(speechText, shouldEndSession, cardText, reprompt) 
+{
+    const speechOutput = "<speak>" + speechText + "</speak>"
+    var jsonObj = {
+        "version": "1.0",
+        "response": {
+        "shouldEndSession": shouldEndSession,
+        "outputSpeech": {
+            "type": "SSML",
+            "ssml": speechOutput
+        },
+        "card": {
         "type": "Simple",
         "title": SKILL_NAME,
         "content": cardText,
         "text": cardText
-      }
+        },
+        "reprompt": {
+        "outputSpeech": {
+            "type": "PlainText",
+            "text": reprompt,
+            "ssml": reprompt
+        }
+        }
     }
-  }
-  return jsonObj
-}
-
-function buildResponseWithRepromt(speechText, shouldEndSession, cardText, reprompt) {
-
-  const speechOutput = "<speak>" + speechText + "</speak>"
-  var jsonObj = {
-     "version": "1.0",
-     "response": {
-      "shouldEndSession": shouldEndSession,
-       "outputSpeech": {
-         "type": "SSML",
-         "ssml": speechOutput
-       },
-     "card": {
-       "type": "Simple",
-       "title": SKILL_NAME,
-       "content": cardText,
-       "text": cardText
-     },
-     "reprompt": {
-       "outputSpeech": {
-         "type": "PlainText",
-         "text": reprompt,
-         "ssml": reprompt
-       }
-     }
-   }
- }
-  return jsonObj
+    }
+    return jsonObj;
 }
 
