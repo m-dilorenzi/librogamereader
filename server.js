99a616434ee87bc8b11a030190eef6937a157cc9
Matteo Di Lorenzi <matteo.dilorenzi98@gmail.com>
2020-08-13 18:33:01 +0200

    Aggiunti messaggi di log per debug

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ function stopAndExit()
 
 function getRandomNumber(numInf, numSup)
 {
-    var number = Math.floor(Math.random() * (numSup - numInf + 1) + numInf);
+    var number = Math.floor(Math.random() * (numSup - numInf + 1)) + numInf;
     const speechOutput = WHISPER + 'Il tuo numero casuale tra ' + numInf + ' e ' + numSup + ' è ' + number + PAUSE;
     return buildResponseWithRepromt(speechOutput, false, '', '');
 }
